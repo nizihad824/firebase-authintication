43b9bce72cf4fcc55fa7effc83934c6bd6271981
nizihad824 <mdnurulislam97@gmail.com>
2022-04-19 13:05:07 +0200

    set button to get user log in

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 
 import './App.css'
 import app from './firebase.init';
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const auth = getAuth(app)
 
 function App() {
+  const provider = new GoogleAuthProvider();
+
+  const handleGoogleSignIn =()=>{
+    signInWithPopup(auth, provider)
+    .then(result =>{
+      const user = result.user;
+      console.log(user);
+
+
+    }
+      )
+    .catch(error=>{
+      console.log('error',error);
+    })
+  }
+ 
+   
   return (
-    <div>
+    <div className='App'>
+      <button onClick={handleGoogleSignIn}>Google Sign In</button>
 
     </div>
   );
+
+ 
 }
 
 export default App;
